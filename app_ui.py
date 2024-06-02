b4afeb534393ae8843a6701fd0263da887a0bae5
Pushkar-Adari <101546155+Pushkar-Adari@users.noreply.github.com>
2024-06-03 00:45:21 +0530

    rearranged

--- app_ui.py
+++ app_ui.py
@@ -32,11 +32,11 @@ class Ui_Home(object):
         self.StartStop.setFont(font)
         self.StartStop.setContextMenuPolicy(QtCore.Qt.DefaultContextMenu)
         self.StartStop.setStyleSheet("background-color: rgba(43, 11, 11,0.5);\n"
-                                        "border-radius:20;\n"
-                                        "color: rgb(249,35,35);\n"
-                                        "font: 63 14.5pt \"Poppins SemiBold\";\n"
-                                        "\n"
-                                        "")
+                                     "border-radius:20;\n"
+                                     "color: rgb(249,35,35);\n"
+                                     "font: 63 14.5pt \"Poppins SemiBold\";\n"
+                                     "\n"
+                                     "")
         self.StartStop.setCheckable(True)
         self.StartStop.setChecked(False)
         self.StartStop.setObjectName("StartStop")
@@ -61,8 +61,8 @@ class Ui_Home(object):
         self.Profile = QtWidgets.QToolButton(self.Central)
         self.Profile.setGeometry(QtCore.QRect(663, 33, 43, 43))
         self.Profile.setStyleSheet("background-color: rgb(19, 19, 19);\n"
-"border-radius:21px;\n"
-"icon-size:25px;")
+                                   "border-radius:21px;\n"
+                                   "icon-size:25px;")
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/newPrefix/assets/user.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.Profile.setIcon(icon)
@@ -73,8 +73,8 @@ class Ui_Home(object):
         self.Setting = QtWidgets.QToolButton(self.Central)
         self.Setting.setGeometry(QtCore.QRect(718, 33, 43, 43))
         self.Setting.setStyleSheet("background-color: rgb(19, 19, 19);\n"
-"border-radius:21px;\n"
-"icon-size:25px;")
+                                   "border-radius:21px;\n"
+                                   "icon-size:25px;")
         icon1 = QtGui.QIcon()
         icon1.addPixmap(QtGui.QPixmap(":/newPrefix/assets/setting.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.Setting.setIcon(icon1)
@@ -85,7 +85,7 @@ class Ui_Home(object):
         self.Avg = QtWidgets.QFrame(self.Central)
         self.Avg.setGeometry(QtCore.QRect(502, 108, 267, 111))
         self.Avg.setStyleSheet("background-color: rgba(19, 19, 19,    0.7);\n"
-                                "border-radius:20px;")
+                               "border-radius:20px;")
         self.Avg.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.Avg.setFrameShadow(QtWidgets.QFrame.Raised)
         self.Avg.setObjectName("Avg")
@@ -98,8 +98,8 @@ class Ui_Home(object):
         self.label.setFont(font)
         self.label.setAutoFillBackground(False)
         self.label.setStyleSheet("background-color: rgba(255, 255, 255, 0);\n"
-"color:white;\n"
-"")
+                                 "color:white;\n"
+                                 "")
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
 
@@ -108,7 +108,7 @@ class Ui_Home(object):
         self.Week = QtWidgets.QFrame(self.Central)
         self.Week.setGeometry(QtCore.QRect(502, 230, 267, 111))
         self.Week.setStyleSheet("background-color: rgba(19, 19, 19,    0.7);\n"
-"border-radius:20px;")
+                                "border-radius:20px;")
         self.Week.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.Week.setFrameShadow(QtWidgets.QFrame.Raised)
         self.Week.setObjectName("Week")
@@ -120,7 +120,7 @@ class Ui_Home(object):
         font.setItalic(True)
         self.label_2.setFont(font)
         self.label_2.setStyleSheet("background-color: rgba(0,0,0,0);\n"
-"color:white;")
+                                   "color:white;")
         self.label_2.setAlignment(QtCore.Qt.AlignCenter)
         self.label_2.setObjectName("label_2")
 
@@ -129,7 +129,7 @@ class Ui_Home(object):
         self.Graph = QtWidgets.QFrame(self.Central)
         self.Graph.setGeometry(QtCore.QRect(31, 108, 458, 329))
         self.Graph.setStyleSheet("background-color: rgba(19, 19, 19,    0.7);\n"
-"border-radius:20px;")
+                                 "border-radius:20px;")
         self.Graph.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.Graph.setFrameShadow(QtWidgets.QFrame.Raised)
         self.Graph.setObjectName("Graph")
@@ -145,69 +145,71 @@ class Ui_Home(object):
         font.setItalic(False)
         self.TimerLabel.setFont(font)
         self.TimerLabel.setStyleSheet("color:white;\n"
-"font-size: 14.5pt;\n"
-"background-color: rgba(0, 0, 0, 0);")
+                                      "font-size: 14.5pt;\n"
+                                      "background-color: rgba(0, 0, 0, 0);")
         self.TimerLabel.setFrameShape(QtWidgets.QFrame.NoFrame)
         self.TimerLabel.setFrameShadow(QtWidgets.QFrame.Plain)
         self.TimerLabel.setWordWrap(False)
         self.TimerLabel.setObjectName("TimerLabel")
+
         self.Pause = QtWidgets.QToolButton(self.Central)
         self.Pause.setGeometry(QtCore.QRect(678, 498, 43, 43))
         self.Pause.setStyleSheet("background-color: rgb(19, 19, 19);\n"
-"border-radius:21px;\n"
-"icon-size:20px;")
+                                 "border-radius:21px;\n"
+                                 "icon-size:20px;")
         icon2 = QtGui.QIcon()
         icon2.addPixmap(QtGui.QPixmap(":/newPrefix/assets/play.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         icon2.addPixmap(QtGui.QPixmap(":/newPrefix/assets/pause.png"), QtGui.QIcon.Normal, QtGui.QIcon.On)
-
         self.Pause.setIcon(icon2)
         self.Pause.setCheckable(True)
         self.Pause.setChecked(False)
         self.Pause.setObjectName("Pause")
         self.Pause.toggled.connect(self.toggle_timer)
 
-        self.timer = QTimer()
-        self.timer.setInterval(1000)
-        self.timer.timeout.connect(self.update_progress)
         self.Reset = QtWidgets.QToolButton(self.Central)
         self.Reset.setGeometry(QtCore.QRect(726, 498, 43, 43))
         self.Reset.setStyleSheet("background-color: rgb(19, 19, 19);\n"
-"border-radius:21px;\n"
-"icon-size:20px;")
+                                 "border-radius:21px;\n"
+                                 "icon-size:20px;")
         icon3 = QtGui.QIcon()
         icon3.addPixmap(QtGui.QPixmap(":/newPrefix/assets/reset.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
         self.Reset.setIcon(icon3)
         self.Reset.setObjectName("Reset")
         self.Reset.clicked.connect(self.reset_timer)
+
         self.Name = QtWidgets.QFrame(self.Central)
         self.Name.setGeometry(QtCore.QRect(586, 38, 119, 32))
         self.Name.setStyleSheet("background-color: rgba(19, 19, 19,    0.7);\n"
-"border-radius:15px\n"
-";")
+                                "border-radius:15px\n"
+                                ";")
         self.Name.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.Name.setFrameShadow(QtWidgets.QFrame.Raised)
         self.Name.setObjectName("Name")
+
         self.Background = QtWidgets.QFrame(self.Central)
         self.Background.setGeometry(QtCore.QRect(0, 0, 800, 600))
         self.Background.setStyleSheet("border-image: url(:/newPrefix/b2.png) 0 0 0 0 stretch stretch;\n"
-"border-width: 0px;\n"
-"")
+                                      "border-width: 0px;\n"
+                                      "")
         self.Background.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.Background.setFrameShadow(QtWidgets.QFrame.Raised)
         self.Background.setObjectName("Background")
+
+        #///////// PROGRESS BARS /////////
+
         self.min10 = QtWidgets.QProgressBar(self.Central)
         self.min10.setGeometry(QtCore.QRect(32, 498, 106, 43))
         self.min10.setStyleSheet("QProgressBar{\n"
-"background-color:rgba(45,45,45,0.5);\n"
-"border-radius:21px;\n"
-"text-align:center;\n"
-"color:transparent;\n"
-"}\n"
-"QProgressBar::chunk{\n"
-"background:qLineargradient(spread:pad, x1:0, y1:0, x2:1, x2:1, stop:0 rgba(52,30,63,255),stop:1 rgba(134,89,157,255));\n"
-"border-radius:21px;\n"
-"}")
-        self.min10.setRange(0,self.min10total)
+                                 "background-color:rgba(45,45,45,0.5);\n"
+                                 "border-radius:21px;\n"
+                                 "text-align:center;\n"
+                                 "color:transparent;\n"
+                                 "}\n"
+                                 "QProgressBar::chunk{\n"
+                                 "background:qLineargradient(spread:pad, x1:0, y1:0, x2:1, x2:1, stop:0 rgba(52,30,63,255),stop:1 rgba(134,89,157,255));\n"
+                                 "border-radius:21px;\n"
+                                 "}")
+        self.min10.setRange(0, self.min10total)
         self.min10.setValue(self.min10actual)
         self.min10.setObjectName("min10")
         bm = QBitmap(self.min10.size())
@@ -217,24 +219,22 @@ class Ui_Home(object):
         p.setPen(Qt.color1)
         p.setBrush(Qt.color1)
         radius = 21
-        p.drawRoundedRect(QRect(0,0,bm.width(),bm.height()),radius,radius)
+        p.drawRoundedRect(QRect(0, 0, bm.width(), bm.height()), radius, radius)
         p.end()
         self.min10.setMask(bm)
-
-        # 50 min timer
 
         self.min50 = QtWidgets.QProgressBar(self.Central)
         self.min50.setGeometry(QtCore.QRect(143, 498, 530, 43))
         self.min50.setStyleSheet("QProgressBar{\n"
-"background-color:rgba(45,45,45,0.5);\n"
-"border-radius:21px;\n"
-"text-align:center;\n"
-"color:transparent;\n"
-"}\n"
-"QProgressBar::chunk{\n"
-"background:qLineargradient(spread:pad, x1:0, y1:0, x2:1, x2:1, stop:0 rgba(50,51,113,255),stop:1 rgba(2,19,51,255));\n"
-"border-radius:21px;\n"
-"}")
+                                 "background-color:rgba(45,45,45,0.5);\n"
+                                 "border-radius:21px;\n"
+                                 "text-align:center;\n"
+                                 "color:transparent;\n"
+                                 "}\n"
+                                 "QProgressBar::chunk{\n"
+                                 "background:qLineargradient(spread:pad, x1:0, y1:0, x2:1, x2:1, stop:0 rgba(50,51,113,255),stop:1 rgba(2,19,51,255));\n"
+                                 "border-radius:21px;\n"
+                                 "}")
         self.min50.setRange(0, self.min50total)
         self.min50.setValue(self.min50total)
         self.min50.setObjectName("min50")
@@ -245,7 +245,7 @@ class Ui_Home(object):
         p.setPen(Qt.color1)
         p.setBrush(Qt.color1)
         radius = 21
-        p.drawRoundedRect(QRect(0,0,bm.width(),bm.height()),radius,radius)
+        p.drawRoundedRect(QRect(0, 0, bm.width(), bm.height()), radius, radius)
         p.end()
         self.min50.setMask(bm)
 
@@ -268,9 +268,15 @@ class Ui_Home(object):
         self.retranslateUi(Home)
         QtCore.QMetaObject.connectSlotsByName(Home)
 
+        #///////// TIMER SETUP /////////
+        
+        self.timer = QTimer()
+        self.timer.setInterval(1000)
+        self.timer.timeout.connect(self.update_progress)
+
     def retranslateUi(self, Home):
         _translate = QtCore.QCoreApplication.translate
-        Home.setWindowTitle(_translate("Home", "MainWindow"))
+        Home.setWindowTitle(_translate("Home", "Just Blink"))
         self.StartStop.setText(_translate("Home", "Stop Tracking"))
         self.Logo.setText(_translate("Home", "Just Blink"))
         self.Profile.setText(_translate("Home", "..."))
@@ -280,37 +286,41 @@ class Ui_Home(object):
         self.TimerLabel.setText(_translate("Home", "Pomodoro Timer"))
         self.Pause.setText(_translate("Home", "..."))
         self.Reset.setText(_translate("Home", "..."))
+
+    #///////// FUNCTIONALITY /////////
+
     def changePlayIcon(self):
         if self.Pause.icon().name() == "play.png":
             self.Pause.setIcon(QIcon(":/newPrefix/assets/pause.png"))
         else:
             self.Pause.setIcon(QIcon(":/newPrefix/assets/play.png"))
-    def toggle_timer(self,checked):
+
+    def toggle_timer(self, checked):
         if checked:
             self.timer.start()
             self.Pause.setIcon(QIcon(":/newPrefix/assets/pause.png"))
         else:
             self.timer.stop()
             self.Pause.setIcon(QIcon(":/newPrefix/assets/play.png"))
+
     def update_progress(self):
         self.min50actual -= 1
         self.min50.setValue(self.min50actual)
         if self.min50actual <= 0:
             self.update_restProgress()
+
     def update_restProgress(self):
         self.min10actual -= 1
         self.min10.setValue(self.min10actual)
         if self.min10actual <= 0:
-           #self.Pause.setChecked(False)
-           self.reset_timer()
+            self.reset_timer()
+
     def reset_timer(self):
-        #maybe add a loop to pass values to the bars so it "fills up" gradually
         self.timer.stop()
         self.min50actual = self.min50total
         self.min50.setValue(self.min50actual)
         self.min10actual = self.min10total
         self.min10.setValue(self.min10actual)
         self.Pause.setChecked(False)
-
 
 import rc_rc
